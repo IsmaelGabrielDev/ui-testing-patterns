cbe74ce5de70aefffbbcbac8762e9595f374f381
Ismael Gabriel <ismaelgabriel31@gmail.com>
2024-12-24 10:32:25 -0300

    Teste de UI com Cypress utilizando o Cucumber e BDD

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,5 +1,5 @@
-const { homePage } = require("../support/pages/home.page")
-const loginPage = require("../support/pages/login.page")
+import { homePage } from "../support/pages/home.page"
+import loginPage from "../support/pages/login.page"
 
 Cypress.Commands.add('login', (email, senha) => { 
     cy.setCookie('ebacStoreVersion', 'v2', { domain: 'lojaebac.ebaconline.art.br' })
